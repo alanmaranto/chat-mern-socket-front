c85fffc2751f6e11de9b00aeef3738d80694acf2
alan <alanarostegui@hotmail.com>
2021-02-11 14:40:34 -0600

    feature(login page): add functions to submit form and states

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    rememberme: false,
+  });
+
+  const onChange = ({ target: { name, value } }) => {
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const toggleCheck = () => {
+    setForm({
+      ...form,
+      rememberme: !form.rememberme,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="login100-form validate-form flex-sb flex-w">
+    <form
+      className="login100-form validate-form flex-sb flex-w"
+      onSubmit={onSubmit}
+    >
       <span className="login100-form-title mb-3">Chat - Ingreso</span>
 
       <div className="wrap-input100 validate-input mb-3">
@@ -12,6 +39,8 @@ const Login = () => {
           type="email"
           name="email"
           placeholder="Email"
+          value={form.email}
+          onChange={onChange}
         />
         <span className="focus-input100"></span>
       </div>
@@ -22,17 +51,21 @@ const Login = () => {
           type="password"
           name="password"
           placeholder="Password"
+          value={form.password}
+          onChange={onChange}
         />
         <span className="focus-input100"></span>
       </div>
 
       <div className="row mb-3">
-        <div className="col">
+        <div className="col" onClick={() => toggleCheck()}>
           <input
             className="input-checkbox100"
             id="ckb1"
             type="checkbox"
-            name="remember-me"
+            name="rememberme"
+            checked={form.rememberme}
+            readOnly
           />
           <label className="label-checkbox100">Recordarme</label>
         </div>
