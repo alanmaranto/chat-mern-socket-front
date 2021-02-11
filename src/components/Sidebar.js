87d91c6015fc14d36b4278e770cb258895d402d4
alan <alanarostegui@hotmail.com>
2021-02-11 11:57:50 -0600

    refactor(sidebar): split sidebar and sidebarchatitem

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,13 @@
 import React from "react";
+import SidebarChatItem from "./SidebarChatItem";
 
 const Sidebar = () => {
+  const chats = [1, 2, 3, 4];
   return (
     <div className="inbox_chat">
-      {/*                     <!-- conversación activa inicio -->
-       */}{" "}
-      <div className="chat_list active_chat">
-        <div className="chat_people">
-          <div className="chat_img">
-            <img
-              src="https://ptetutorials.com/images/user-profile.png"
-              alt="sunil"
-            />
-          </div>
-          <div className="chat_ib">
-            <h5>Some random name</h5>
-            <span className="text-success">Online</span>
-            <span className="text-danger">Offline</span>
-          </div>
-        </div>
-      </div>
-      {/*                     <!-- conversación activa Fin -->
-       */}
+      {chats.map((chat) => (
+        <SidebarChatItem key={chat} />
+      ))}
       {/*                     <!-- conversación inactiva inicio -->
        */}{" "}
       <div className="chat_list">
