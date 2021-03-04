47d2d28f84ee889fac2a86e5204afe08c15740e5
alan <alanarostegui@hotmail.com>
2021-03-03 19:37:12 -0600

    feature(chat): wrap app into chat context and setting initial state in chat reducer

---
+++ src/context/chat/ChatContext.js
@@ -0,0 +1,27 @@
+import React, { useReducer, createContext } from "react";
+import { chatReducer } from "../../reducers/chat";
+
+export const ChatContext = createContext();
+
+const initialState = {
+  uid: "",
+  activeChat: null, //uid del user al que le enviaré el msj
+  users: [], // todos los users de la db
+  messages: [], // Msj del chat seleccionado
+};
+
+const ChatProvider = ({ children }) => {
+  const [chatState, dispatch] = useReducer(chatReducer, initialState);
+  return (
+    <ChatContext.Provider
+      value={{
+        chatState,
+        dispatch,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export default ChatProvider;
