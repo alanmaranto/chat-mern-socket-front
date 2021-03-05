08d1340a61de959e6bd4fe3f05cae1c9ec81abf8
alan <alanarostegui@hotmail.com>
2021-03-05 08:50:04 -0600

    sidebar: remove mock code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,36 +8,15 @@ const Sidebar = () => {
   const { auth } = useContext(AuthContext);
 
   const { users } = chatState;
-  const { uid } = auth
+  const { uid } = auth;
 
   return (
     <div className="inbox_chat">
-      {users.filter((user) => user.uid !== uid).map((user) => (
-        <SidebarChatItem key={user.uid} user={user} />
-      ))}
-      {/*                     <!-- conversación inactiva inicio -->
-       */}{" "}
-      <div className="chat_list">
-        <div className="chat_people">
-          <div className="chat_img">
-            <img
-              src="https://ptetutorials.com/images/user-profile.png"
-              alt="sunil"
-            />
-          </div>
-          <div className="chat_ib">
-            <h5>
-              Sunil Rajput <span className="chat_date">Dec 25</span>
-            </h5>
-            <p>
-              Test, which is a new approach to have all solutions astrology
-              under one roof.
-            </p>
-          </div>
-        </div>
-      </div>
-      {/*                     <!-- conversación inactiva inicio -->
-       */}
+      {users
+        .filter((user) => user.uid !== uid)
+        .map((user) => (
+          <SidebarChatItem key={user.uid} user={user} />
+        ))}
       {/*                     <!-- Espacio extra para scroll -->
        */}{" "}
       <div className="extra_space"></div>
